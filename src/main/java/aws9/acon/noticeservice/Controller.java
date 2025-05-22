cc49208e83c544a1a8e61fa86f177c7219aa7fc9
seminchoi <choicco89@gmail.com>
2025-05-22 14:34:27 +0900

    feat: center API 생성

--- src/main/java/aws9/acon/noticeservice/Controller.java
+++ src/main/java/aws9/acon/noticeservice/Controller.java
@@ -1,42 +1,64 @@
 package aws9.acon.noticeservice;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 @RestController
 @RequiredArgsConstructor
 public class Controller {
-    private final Repository repository;
     private final ServicesUrl servicesUrl;
 
     private RestTemplate restTemplate = new RestTemplate();
     private Random random = new Random();
 
     // Health check
-    @GetMapping
+    @GetMapping("/healthz")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("healthy");
     }
-    
-    @GetMapping("/notices")
-    public ResponseEntity<List<Sample>> getList() {
-        int page = random.nextInt(10_000);
-        int id = random.nextInt(1_000_000);
 
-        if(id % 3 == 0) {
-            Sample sample = restTemplate.getForObject(servicesUrl.getCentral() + "/" + id, Sample.class);
-            String content = sample.getContent();
+    @GetMapping("/")
+    public ResponseEntity<Sample> getSample() {
+        random();
+        return ResponseEntity.ok(generateSample());
+    }
+
+
+    public static Sample generateSample() {
+        return Sample.builder()
+                .id(new Random().nextInt(10000))
+                .title("샘플 제목입니다.")
+                .content(generateNumericString(10_000))
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    private static String generateNumericString(int length) {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            sb.append(random.nextInt(10)); // 0 ~ 9
         }
 
-        Pageable pageable = PageRequest.of(page, 100);
-        return ResponseEntity.ok(repository.findAll(pageable).map(n -> n).toList());
+        return sb.toString();
+    }
+
+
+    private void random() {
+        List<Integer> randoms = new ArrayList<>();
+        for (int i = 0; i < 3000; i++) {
+            int randInt = random.nextInt();
+            randoms.add(randInt);
+        }
+        randoms.sort(Integer::compareTo);
     }
 }
